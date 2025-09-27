52c4c7c1fdf40f14c81495b2a3ca42f92422b86f
kirtiiiiii110 <105166751+kirtiiiiii110@users.noreply.github.com>
2025-09-27 23:38:32 +0530

    lop

---
+++ contracts/limit-order-protocol/deploy/deploy.js
@@ -0,0 +1,34 @@
+const hre = require("hardhat");
+
+const wethByNetwork = {
+    hardhat: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    mainnet: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    monad: "0x760AfE86e5de5fa0Ee542fc7B7B713e1c5425701",
+};
+
+const deploy = async () => {
+    const { deployments, getNamedAccounts, getChainId, network } = hre;
+
+    console.log("running deploy script");
+    console.log("network id ", await getChainId());
+
+    const { deploy } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    const limitOrderProtocol = await deploy("LimitOrderProtocol", {
+        from: deployer,
+        args: [wethByNetwork[network.name]],
+    });
+
+    console.log("LimitOrderProtocol deployed to:", limitOrderProtocol.address);
+
+    if ((await getChainId()) !== "31337") {
+        await hre.run("verify:verify", {
+            address: limitOrderProtocol.address,
+            constructorArguments: [wethByNetwork[network.name]],
+        });
+    }
+};
+deploy();
+
+// module.exports.skip = async () => true;
---
+++ contracts/limit-order-protocol/hardhat.config.js
@@ -0,0 +1,82 @@
+require("@matterlabs/hardhat-zksync-deploy");
+require("@matterlabs/hardhat-zksync-solc");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("solidity-coverage");
+require("solidity-docgen");
+require("hardhat-dependency-compiler");
+require("hardhat-deploy");
+require("hardhat-gas-reporter");
+require("hardhat-tracer");
+require("dotenv").config();
+const { oneInchTemplates } = require("@1inch/solidity-utils/docgen");
+const { Networks, getNetwork } = require("@1inch/solidity-utils/hardhat-setup");
+
+if (getNetwork().indexOf("zksync") !== -1) {
+    require("@matterlabs/hardhat-zksync-verify");
+} else {
+    require("@nomicfoundation/hardhat-verify");
+}
+
+const networksConfig = new Networks();
+networksConfig.registerCustom(
+    "monad",
+    10143,
+    "https://testnet-rpc.monad.xyz",
+    "<PRIVATE_KEY>",
+    "MONAD_ETHERSCAN_KEY",
+    "https://sourcify-api-monad.blockvision.org",
+    "https://testnet.monadexplorer.com"
+);
+const { networks, etherscan } = networksConfig;
+console.log({ networks, etherscan });
+
+module.exports = {
+    etherscan,
+    tracer: {
+        enableAllOpcodes: true,
+    },
+    solidity: {
+        version: "0.8.30",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 1_000_000,
+            },
+            evmVersion: networks[getNetwork()]?.hardfork || "cancun",
+            viaIR: true,
+        },
+    },
+    networks,
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
+    sourcify: {
+        enabled: true,
+        apiUrl: "https://sourcify-api-monad.blockvision.org",
+        browserUrl: "https://testnet.monadexplorer.com",
+    },
+    gasReporter: {
+        enable: true,
+        currency: "USD",
+    },
+    dependencyCompiler: {
+        paths: [
+            "@1inch/solidity-utils/contracts/mocks/TokenCustomDecimalsMock.sol",
+            "@1inch/solidity-utils/contracts/mocks/TokenMock.sol",
+            "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol",
+        ],
+    },
+    zksolc: {
+        version: "1.4.0",
+        compilerSource: "binary",
+        settings: {},
+    },
+    docgen: {
+        outputDir: "docs",
+        templates: oneInchTemplates(),
+        pages: "files",
+        exclude: ["mocks"],
+    },
+};
